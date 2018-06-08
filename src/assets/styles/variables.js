7a7d6d922076d9ac8ecb9596151678f07bb0abf6
Anzhelika Nekrasova <Angel-Nek@mail.ru>
2018-06-08 17:32:52 +0300

    page about

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,7 +1,8 @@
 module.exports = {
   white: "#efeee9",
   beige: "#eeebdf",
-  green: "#16c2aa",
+  green: "#6fd1c6",
+  "green-hover": "#00bfa5",
   "text-color": "#455a64",
   grey: "#373e42",
   "light-grey": "#dfdcd5"
