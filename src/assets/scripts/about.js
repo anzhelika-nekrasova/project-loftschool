81e2aa278838f5a00e478d5411eed6a370e9484c
Anzhelika Nekrasova <Angel-Nek@mail.ru>
2018-06-25 07:10:28 +0300

    skills done

--- src/assets/scripts/about.js
+++ src/assets/scripts/about.js
@@ -1,1 +1,3 @@
 import "./modules/parallax";
+import "./modules/skills";
+import "./modules/hamburger";
--- src/assets/scripts/modules/skills.js
+++ src/assets/scripts/modules/skills.js
@@ -22,27 +22,28 @@ const skill = {
     }
   };
   
-  const skillsRow = {
-    template: "#skills-row",
-    components: {
-      skill
-    },
-    props: {
-      skill: Object
-    }
-  };
-  
-  new Vue({
-    el: "#skills-container",
-    components: {
-      skillsRow
-    },
-    data: {
-      skills: {}
-    },
-    created() {
-      const data = require("../../../data/skills.json");
-      this.skills = data;
-    },
-    template: "#skills-list"
-  });
+const skillsRow = {
+template: "#skills-row",
+components: {
+    skill
+},
+props: {
+    skill: Object
+}
+};
+
+new Vue({
+el: "#skills-container",
+components: {
+    skillsRow
+},
+data: {
+    skills: {}
+},
+created() {
+    const data = require("../../../data/skills.json");
+    this.skills = data;
+    console.log(data);
+},
+template: "#skills-list"
+});
