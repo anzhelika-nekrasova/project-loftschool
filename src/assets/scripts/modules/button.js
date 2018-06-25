393460499cbcd24d5c7f8e43fba9eb09c0f9a0ee
Anzhelika Nekrasova <Angel-Nek@mail.ru>
2018-06-25 12:19:01 +0300

    scroll

--- src/assets/scripts/modules/button.js
+++ src/assets/scripts/modules/button.js
@@ -1,6 +1,12 @@
 $(function() {
-    $(".header__down-btns").click(function() {
-        $('body').animate({ scrollTop: '+=' + window.innerHeight }, 'slow');
-        return false;
+    $(".header__down-btns").click(function (e) {
+        var heightScroll = window.innerHeight - 30;
+        $('body,html').animate({ scrollTop: heightScroll }, 1500);
+        e.preventDefault();
+    });
+
+    $(".up-button").click(function (e) {
+        $('body,html').animate({ scrollTop: 0 }, 1500);
+        e.preventDefault();
     });
 })
