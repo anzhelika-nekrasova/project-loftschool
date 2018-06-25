fc0cb4af75edbc75aac202d765189c41c9e2ff3d
Anzhelika Nekrasova <Angel-Nek@mail.ru>
2018-06-25 15:03:52 +0300

    parallax fron for mobile

--- src/assets/scripts/modules/parallax-front.js
+++ src/assets/scripts/modules/parallax-front.js
@@ -2,24 +2,35 @@
     const DOC = document;
     const WIN = window;
 
-   // WIN.navigator.userAgent
-    const parallaxFrontContainer = DOC.getElementById('parallax-front');
-    const layers = parallaxFrontContainer.children;
+    if( navigator.userAgent.match(/Android/i)
+        || navigator.userAgent.match(/webOS/i)
+        || navigator.userAgent.match(/iPhone/i)
+        || navigator.userAgent.match(/iPad/i)
+        || navigator.userAgent.match(/iPod/i)
+        || navigator.userAgent.match(/BlackBerry/i)
+        || navigator.userAgent.match(/Windows Phone/i)
+        ){
+            DOC.getElementsByClassName('parallax-front').classList.add('front-page-bg__phone');
+            DOC.getElementsByClassName('parallax-front').classList.add('parallax-front__disable');
+        }
+    else {
 
-    const moveLayers = e => {
-        const initialX = WIN.innerWidth / 2 - e.pageX;
-        const initialY = WIN.innerHeight / 2 - e.pageY;
+        const parallaxFrontContainer = DOC.getElementById('parallax-front');
+        const layers = parallaxFrontContainer.children;
 
-        Array.from(layers).forEach((layer, i) => {
-            const divider = i / 100;
-            const posX = initialX * divider;
-            const posY = initialY * divider;
+        const moveLayers = e => {
+            const initialX = WIN.innerWidth / 2 - e.pageX;
+            const initialY = WIN.innerHeight / 2 - e.pageY;
 
-            layer.style.transform = `translate(${posX}px, ${posY}px)`;
-        });
-    };
+            Array.from(layers).forEach((layer, i) => {
+                const divider = i / 100;
+                const posX = initialX * divider;
+                const posY = initialY * divider;
 
-    WIN.addEventListener('mousemove', moveLayers);
+                layer.style.transform = `translate(${posX}px, ${posY}px)`;
+            });
+        };
 
-
+        WIN.addEventListener('mousemove', moveLayers);
+        }
 }
